d59b87f067a9916bde3c602ad179cea1f170767b
xiaoxpkuo <121032293@qq.com>
2018-05-30 21:00:14 +0800

    todolist

--- 3day/todo.js
+++ 3day/todo.js
@@ -1,5 +1,6 @@
 const vm = new Vue({
     el: "#app",
+
     data: {
         todos: [
             {
@@ -11,7 +12,9 @@ const vm = new Vue({
                 title: 'eat'
             }
         ],
-        addItemCon:''
+        addItemCon: '',
+        currentItem: '',
+        hash: 'all'
     },
     methods: {
         deleteItem(p){
@@ -20,16 +23,72 @@ const vm = new Vue({
         addItem(){
             this.todos.push({
                 isSelect: false,
-                title:this.addItemCon
+                title: this.addItemCon
             });
-            this.addItemCon='';
+            this.addItemCon = '';
+        },
+        actItem(item){
+            this.currentItem = item;
+        },
+        subEditItem(item){
+            this.currentItem = '';
         }
     },
     computed: {
         allLen(){
             return this.todos.length
+        },
+        filterTodos(){
+            if (this.hash === 'finish') {
+                return this.todos.filter(function (item) {
+                    return item.isSelected
+                })
+            } else if (this.hash === 'unfinish') {
+                return this.todos.filter(function (item) {
+                    return !item.isSelected
+                })
+            } else {
+                return this.todos;
+            }
         }
+    },
+    directives: {
+        focus(el, bindings){
+            //console.log(arguments);
+            if (bindings.value) {
+                //el.focus();
+                //如果是v-if需要延时等待dom显示
+                setTimeout(() => {
+                    el.focus();
+                }, 0)
+            }
+        }
+    },
+    watch: {
+        //watch 默认只监控一层数据
+        /*todos(){ 默认写成函数 相当于只写了handler
+
+         }*/
+        todos: {
+            handler(){
+                localStorage.setItem("todos", JSON.stringify(this.todos));
+            },
+            deep: true
+        }
+    },
+    created(){
+        //赋值之前存储的数据
+        this.todos = JSON.parse(localStorage.getItem("todos")) || this.todos;
+        //获取hash tag 渲染数据
+        window.addEventListener('hashchange', (event) => {
+            console.log(event.newURL);
+            let newHash = event.newURL.slice(event.newURL.indexOf('#/') + 2);
+            console.log(newHash);
+            this.hash = newHash;
+        });
+        this.hash =location.hash.slice(location.hash.indexOf('#/') + 2) || 'all';
     }
+
 });
 
 //1 将数据循环到页面
